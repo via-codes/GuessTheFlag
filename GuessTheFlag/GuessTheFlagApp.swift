c40052c0d942e45290c02eb82257305a50c101c5
Alivia Fairchild <aliviafairchild@Alivias-Air.hsd1.ut.comcast.net>
2022-04-15 09:09:29 -0600

    Initial commit.

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alivia Fairchild on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
